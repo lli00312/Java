12dd1c754be5e0540adc038bd939117d2231d8a8
lli00312 <guineapig1523@naver.com>
2026-06-23 18:07:21 +0900

    Add files via upload

--- p74_OddEven.java
+++ p74_OddEven.java
@@ -1,5 +1,30 @@
 package 이윤서;
 
+import java.util.Scanner;
+
+import java.util.Scanner;
+import java.util.Random;
+
 public class p74_OddEven {
+	public static void main ( String[] args) {
+		
+		Random rnd= new Random();
+		int quest = rnd.nextInt(100);
+		char answer = (quest%2) ==0 ? 'e' :'o';
+		
+		Scanner sc= new Scanner ( System.in);
+		System.out.println("짝수라 생각하면 e를 홀수라 생가갛면 o를 입력 >> ");
+		char guess = sc.next().charAt(0);
+		sc.close();
+		
+		if (guess == answer) {
+			System.out.println("축하합니다!!!");
+		}
+		else {
+			System.out.println("땡");
+	
+		}
+	}
+
 
 }
