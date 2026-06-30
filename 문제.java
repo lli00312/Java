351ea0178b1bfddd08bc09201344cda3678f946a
lli00312 <guineapig1523@naver.com>
2026-06-30 16:10:18 +0900

    Add files via upload

---
+++ 문제.java
@@ -0,0 +1,21 @@
+package 이윤서;
+
+public class 문제 {
+	    public static void main(String[] args){
+	        String str = "41389";
+	        int sum = 0;
+	        for (int i=0; i <str.length(); i++){
+	            sum = sum+ (str.charAt(i));
+	        }
+	        System.out.println("sum= "+sum);
+	    }
+	    }
+
+
+
+for (int i=1; i<=6; i++){
+    for (int x = 1; x <= 6; x++){
+        if (i + x == 6) {
+            System.out.println(i + "+" + x + "=6");
+        }
+}
