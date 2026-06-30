95fdaf7749471878fb101989721f57b96f4b9d46
lli00312 <guineapig1523@naver.com>
2026-06-30 16:16:13 +0900

    Update 문제.java

--- 문제.java
+++ 문제.java
@@ -3,9 +3,9 @@ package 이윤서;
 public class 문제 {
 	    public static void main(String[] args){
 	        String str = "41389";
-	        int sum = 0;
-	        for (int i=0; i <str.length(); i++){
-	            sum = sum+ (str.charAt(int i));
+	        int sum=0;
+	        for (int i=0;i<str.length(); i++){
+	            sum= sum+ (str.charAt(int i));
 	        }
 	        System.out.println("sum= "+sum);
 	    }
@@ -13,9 +13,9 @@ public class 문제 {
 
 
 
-for (int i=1; i<=6; i++){
-    for (int x = 1; x <= 6; x++){
-        if (i + x == 6) {
-            System.out.println(i + "+" + x + "=6");
+for (int i=1; i<=6;i++){
+    for (int x=1; x<=6; x++){
+        if (i+x =6) {
+            System.out.println( i+ "+" +x+ "=6");
         }
 }
