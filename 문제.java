3087c0ce7ea2cd83478408b3a39347460184b6ae
lli00312 <guineapig1523@naver.com>
2026-06-30 19:52:13 +0900

    Add files via upload

---
+++ 문제.java
@@ -0,0 +1,79 @@
+package 이윤서;
+
+// import java.util.Scanner;
+
+public class 문제 {
+	public static void main(String[] args) {
+
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.println("영문자를 입력하세요. (대소문자 상관 없음)");
+//		char ch= sc.next().charAt(0);
+//		if ((ch >= 'A' && ch<= 'Z') || (ch>= 'a' && ch<='z')){
+//			System.out.println("[1-1]5. ");
+//		}
+//		
+//		sc.close();
+
+//		String str = "yes";
+//		if (str.equals("yes")) {
+//			System.out.println("[1-1]8.");
+//		}
+
+//		int num = 1;
+//		int sum = 0;
+//		while (true) {
+//			
+//
+//			if (num % 2 != 0) {
+//				sum = sum + num;
+//			}
+//			else
+//				sum = sum - num;
+//			
+//			
+//			if (sum >= 100) {
+//				break;
+//			}
+//			num++;
+//		}
+//		
+//		System.out.println(num);
+//		
+
+
+		int count=0;
+		while(count<3) {
+			System.out.printf("현재 count : %d %d", count, 3);
+			System.out.println("현재 count : "+ count);
+			System.out.println("count 가 3보다 작은 동안 반복");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		
+//		
+//		
+//		
+//		
+//		
+//		
+//		
+//		
+	}
+}
